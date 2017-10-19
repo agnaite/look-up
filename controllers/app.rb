c4888c1a17ed05ab75a89443f5b9a6e1104ed398
Agne Klimaite <agnaite@me.com>
2017-10-19 14:41:18 -0700

    now parsing response from api

--- controllers/app.rb
+++ controllers/app.rb
@@ -16,5 +16,27 @@ get '/get_products' do
   response = Requests.request("GET", "https://www.dollskill.com/api/v1/catalog/products/"+@products,
                               params: { :op => "get_size_attributes" })
 
-  response.body # Parsed body
+  parse_response(response.body).to_json # Parsed body
+end
+
+# helpers
+
+def parse_response(response)
+  products = JSON.parse(response)
+  parsed_products = {}
+
+  for product in products.keys
+    parsed_products[product] = []
+    for item in products[product]
+      if item.is_a? Array
+        parsed_products[product] <<  {size: item[1]["size_text"]}
+        parsed_products[product] <<  {qty: item[1]["quantity"]}
+      else
+        parsed_products[product] <<  {size: item["size_text"]}
+        parsed_products[product] <<  {qty: item["quantity"]}
+      end
+    end
+
+  end
+  parsed_products
 end
