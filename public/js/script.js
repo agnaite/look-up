6f9f2996e40173a70a23769d8dab7a9d0fa15609
Agne Klimaite <agnaite@me.com>
2017-10-20 14:04:56 -0700

    added handlebars, changed styling

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,38 @@
-const urlStart = 'https://www.dollskill.com/api/v1/catalog/products/';
-const urlEnd = '/?op=get_size_attributes';
-
 $('.btn-danger').click(evt => {
   evt.preventDefault();
-  const input = $('.products').val();
 
-  $.get('/get_products', { products: input }, response => {
-    console.log(response);
+  $('table > tbody').empty();
+
+  $.get('/get_products', { products: $('.products').val() }, response => {
+
+    if (response !== '') {
+
+      const allProducts = JSON.parse(response);
+      const inStock = { products: [], caption: 'In Stock'};
+      const ooStock = { products: [], caption: 'Out of Stock'};
+
+      allProducts.forEach(product => {
+        if (product.qty > 0) {
+          inStock.products.push(product);
+        } else {
+          ooStock.products.push(product);
+        }
+      });
+
+      console.log(inStock)
+
+      const theTemplate = Handlebars.compile($("#table-template").html());
+      if (inStock.products.length > 0) $('.in-stock-table').html(theTemplate(inStock));
+      if (ooStock.products.length > 0) $('.oo-stock-table').html(theTemplate(ooStock));
+    }
   });
 });
 
+// helpers
 
+const sort_by_qty = products => {
+  products.sort((a, b) => {
+    return a.qty - b.qty;
+  });
+  return products
+}
