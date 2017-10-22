a2cd16d92b684b02e50ca16bbdc0162ee9ecc880
Agne Klimaite <agnaite@me.com>
2017-10-22 16:35:22 -0700

    now can sort in stock products by quantity

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,68 @@
 $('.btn-danger').click(evt => {
   evt.preventDefault();
 
-  $('table > tbody').empty();
+  const theTemplate = Handlebars.compile($("#table-template").html());
+
+  $('.in-stock-table').html('');
+  $('.oo-stock-table').html('');
 
   $.get('/get_products', { products: $('.products').val() }, response => {
 
     if (response !== '') {
-
       const allProducts = JSON.parse(response);
-      const inStock = { products: [], caption: 'In Stock'};
-      const ooStock = { products: [], caption: 'Out of Stock'};
 
-      allProducts.forEach(product => {
-        if (product.qty > 0) {
-          inStock.products.push(product);
-        } else {
-          ooStock.products.push(product);
-        }
-      });
+      const inStock = filterOutOfStock(allProducts)[0]
+      const ooStock = filterOutOfStock(allProducts)[1]
 
-      console.log(inStock)
+      sortByQty(inStock.products)
 
-      const theTemplate = Handlebars.compile($("#table-template").html());
       if (inStock.products.length > 0) $('.in-stock-table').html(theTemplate(inStock));
       if (ooStock.products.length > 0) $('.oo-stock-table').html(theTemplate(ooStock));
+
+      $('.sort-btn').on('click', reverseRows)
     }
   });
 });
 
 // helpers
 
-const sort_by_qty = products => {
+const sortByQty = (products) => {
   products.sort((a, b) => {
-    return a.qty - b.qty;
+    return b.qty - a.qty;
   });
   return products
 }
+
+const filterOutOfStock = allProducts => {
+  let inStock = { products: [], caption: 'In Stock', sort: true};
+  let ooStock = { products: [], caption: 'Out of Stock'};
+
+  allProducts.forEach(product => {
+    if (product.qty > 0) {
+      inStock.products.push(product);
+    } else {
+      ooStock.products.push(product);
+    }
+  });
+  return [inStock, ooStock];
+}
+
+const updateSortIcon = () => {
+  if ($('.sort-btn i').hasClass('fa-sort-numeric-desc')) {
+    $('.sort-btn').html('<i class="fa fa-sort-numeric-asc" aria-hidden="true"></i>');
+  } else {
+    $('.sort-btn').html('<i class="fa fa-sort-numeric-desc" aria-hidden="true"></i>');
+  }
+}
+
+const reverseRows = () => {
+  const table = $('.in-stock-table tbody');
+  for (var i = 0; i < table.length; i++) {
+    var rows = table[i].rows;
+    for (var j = 0; j < rows.length; j++) {
+      rows[j].parentNode.insertBefore(rows[rows.length-1], rows[j]);
+    }
+  }
+  updateSortIcon();
+}
+
